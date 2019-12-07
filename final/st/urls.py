270a4e9c2cce38da7af2540c748be7ccbdd619aa
yz3651 <marianazyh@foxmail.com>
2019-12-07 18:12:29 +0000

    Updated App URLs

--- final/st/urls.py
+++ final/st/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('map/', views.showmap, name='showmap'),
+    path('sightings', views.sightings, name='sightings'),
+    path('sightings/stats', views.showstats, name='showstats'),
+    path('sightings/add', views.add, name='add'),
+    path('sightings/<unique_squirrel_id>', views.edit, name='edit'),
 ]
