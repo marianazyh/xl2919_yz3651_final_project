000c9ccd55a4c0c18503f63d6e31149557ff99d0
Xiuzhu Lin <xl2919@columbia.edu>
2019-12-07 18:37:43 +0000

    Change views.py: create showmap function

--- final/st/views.py
+++ final/st/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import st_model
+from .forms import st_form
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+def showmap(request):
+    sightings = st_model.objects.all()
+    context = {
+        'sightings': sightings,
+    }
+    return render(request, 'st/map.html', context)
+
+def sightings(request):
+    return True
+
+def showstats(request):
+    return True
+
+def add(request):
+    return True
+
+def edit(request, unique_squirrel_id):
+    return True
