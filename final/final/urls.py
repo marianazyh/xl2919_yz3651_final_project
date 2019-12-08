b2fb329d2fb1cadb896fd4eb00cb77b5a2d8a293
Xiuzhu Lin <xl2919@columbia.edu>
2019-12-08 05:07:43 +0000

    Modify urls.py

--- final/final/urls.py
+++ final/final/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('st/', include('st.urls')),
+    path('sightings/',include('st.urls')),
     path('admin/', admin.site.urls),
+
 ]
