30c9b06e4bfdb2ead4c76931932672795a7bf3f6
Xiuzhu Lin <xl2919@columbia.edu>
2019-12-08 00:46:35 +0000

    Modify views.py stats function

--- final/st/views.py
+++ final/st/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import st_model
 from .forms import st_form
-
+from django.db.models import Count
 
 
 def showmap(request):
@@ -16,8 +16,27 @@ def sightings(request):
     return True
 
 def showstats(request):
-    return True
-
+    count_squirrles = st_models.objects.value('Unique Squirrel ID').annotate(s_count = Count('Unique Squirrel ID')
+    count_am = st_models.objects.value('Shift').annotate(am_count = Count('AM')
+    count_pm = st_models.objects.value('Shift').annotate(am_count = Count('PM')
+    count_adult = st_models.objects.value('Age').annotate(adult_count = Count('Adult')
+    count_juvenile = st_models.objects.value('Age').annotate(juvenile_count = Count('Juvenile')
+    count_above = st_models.objects.value('Location').annotate(above_count = Count('Above Ground')
+    count_plane = st_models.objects.value('Location').annotate(plane_count = Count('Ground Plane')
+    count_running = st_models.objects.filter('Running'=True).Count()
+    count_eating = st_models.objects.filter('Eating'= True).Count()
+    context = {
+        'count_squirrles':count_squirrles,
+        'count_am':count_am,
+        'count_pm':count_pm,
+        'count_adult':count_adult,
+        'count_juvenile':count_juvenile,
+        'count_above':count_above,
+        'above_plane':count_plane,
+        'count_running':count_running,
+        'count_eating':count_eating,
+        }
+        
 def add(request):
     return True
 
